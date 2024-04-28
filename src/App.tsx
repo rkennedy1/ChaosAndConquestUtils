df29e761ea7202235b6c0a7fa47cb4eef3c4114b
rkennedy1 <rskennedy99@gmail.com>
2024-04-28 10:55:04 -0700

    add google analytics 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Box } from "@mui/material";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_GA4_KEY || "");
 
 function App() {
   return (
